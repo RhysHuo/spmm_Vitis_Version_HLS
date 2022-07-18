92d1ec8307f5366379b0c69cfd78be7a2b5d35d1
RhysHuo <RhysHuo_1120272267@163.com>
2022-07-18 19:16:34 +0100

    Update control_host.sh

--- control_host.sh
+++ control_host.sh
@@ -1,6 +1,6 @@
-git clone https://github.com/RhysHuo/spmm_Vitis_Version.git
-cd spmm_Vitis_Version
+git clone https://github.com/RhysHuo/spmm_Vitis_Version_HLS.git
+cd spmm_Vitis_Version_HLS
 cp host.cpp ..
 cd ..
-rm -rf spmm_Vitis_Version
+rm -rf spmm_Vitis_Version_HLS
 g++ -g -std=c++14 -I$XILINX_XRT/include -L${XILINX_XRT}/lib/ -I/mnt/scratch/rhyhuo/HLS_arbitrary_Precision_Types/include -o host.exe host.cpp spmm_block.h -lOpenCL -pthread -lrt -lstdc++
